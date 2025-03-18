555e0cdf112ef3bd998b558e2b36b80fc0988162
Boniface <boniface.ebuka@gmail.com>
2025-03-18 17:34:44 +0100

    Updated the user model to inlcude "github_id" to store the user's GitHub account ID

--- packages/api/src/models/users/user.rs
+++ packages/api/src/models/users/user.rs
@@ -9,11 +9,17 @@ use by_types::QueryResponse;
 pub struct User {
     #[api_model(primary_key)]
     pub id: i64,
+
     #[api_model(auto = [insert])]
     pub created_at: i64,
+
     #[api_model(auto = [insert, update])]
     pub updated_at: i64,
+
     #[api_model(unique)]
     #[validate(email)]
     pub email: String,
+
+    #[api_model(unique)]
+    pub github_id: String,
 }
